8b89ab50197792ca8211c1670dae7148d819948b
Mauricio Bedoya <bedoyama@gmail.com>
2024-02-14 20:32:12 -0600

    library

---
+++ ch03/stand_by1.py
@@ -0,0 +1,10 @@
+from mysr import voice_to_text
+
+while True:
+    print('Python is listening...')
+    inp = voice_to_text() 
+    print(f'You just said {inp}.')
+    if inp == "stop listening":
+        print('Goodbye!')
+        break
+
